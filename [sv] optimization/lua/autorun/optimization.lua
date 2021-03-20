61c9052dde61fcf0650aef15bcd3c8a82bc975ed
aktarda <67874428+aktarda@users.noreply.github.com>
2021-03-20 15:03:33 +0300

    Update optimization.lua

--- [sv] optimization/lua/autorun/optimization.lua
+++ [sv] optimization/lua/autorun/optimization.lua
@@ -1,1 +1,2 @@
+--
 function OptimizationSpawn( ply ) 	ply:ConCommand("gmod_mcore_test 1; mat_queue_mode -1; cl_threaded_bone_setup 1") 	ply:ConCommand("cl_threaded_client_leaf_system 1") 	ply:ConCommand("r_threaded_client_shadow_manager 1") 	ply:ConCommand("r_threaded_particles 1") 	ply:ConCommand("r_threaded_renderables 1") 	ply:ConCommand("studio_queue_mode 1") 	ply:ConCommand("r_queued_ropes 1") 	ply:ConCommand("cl_cmdrate +1000") 	ply:ConCommand("fps_max 0")	ply:ConCommand("rate 1280000") end hook.Add( "PlayerInitialSpawn", "OptimizationSpawn", OptimizationSpawn )
